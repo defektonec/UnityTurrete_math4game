62804c422aa245279bf61583519895abfdf8cba0
defektonec <sarjovskiy.dima@gmail.com>
2025-04-29 16:50:12 +0200

    turrete calculating nearest target

--- Assets/Scripts/TurretScript.cs
+++ Assets/Scripts/TurretScript.cs
@@ -9,6 +9,7 @@ public class TurretScript : MonoBehaviour
 {
     [SerializeField][Range(20f, 180f)] private float turretHorizontalAngle;
     [SerializeField][Range(0f, 90)] private float turretVerticalAngle;
+    [SerializeField][Range(0.1f, 2)] private float turreteRotationSpeed;
     [SerializeField] private string[] targetTags;
 
     private float currentHorizontalAngle;
@@ -17,8 +18,8 @@ public class TurretScript : MonoBehaviour
     private Vector3 directionToTarget;
     private Quaternion defaultLookingDirection;
 
-    private List<GameObject> enemiesList = new List<GameObject>();
-    private Transform currentEnemyTransform;
+    private List<GameObject> targetList = new List<GameObject>();
+    private Transform currentTargetTransform;
 
     void Start()
     {
@@ -39,12 +40,15 @@ public class TurretScript : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, defaultLookingDirection, Time.deltaTime * 2f);
         }
         // If there is no enemy in list just return
-        if (enemiesList.Count == 0) 
+        if (targetList.Count == 0) 
             return;
-        else 
-            currentEnemyTransform = enemiesList[0].transform;
+        else
+        {
+            CalculateTheNearestTarget();
+            currentTargetTransform = targetList[0].transform;
+        }
 
-        directionToTarget = currentEnemyTransform.position - transform.position;
+        directionToTarget = currentTargetTransform.position - transform.position;
 
         Vector3 flatDirection = new Vector3(directionToTarget.x, 0f, directionToTarget.z);
         currentHorizontalAngle = Vector3.Angle(transform.forward, flatDirection.normalized);
@@ -52,33 +56,56 @@ public class TurretScript : MonoBehaviour
 
         if (CheckEnemyVisibility())
         {
-            Quaternion targetRotation = Quaternion.LookRotation(currentEnemyTransform.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2f);
+            Quaternion targetRotation = Quaternion.LookRotation(currentTargetTransform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turreteRotationSpeed);
         }
     }
 
     private bool CheckEnemyVisibility()
     {
+        // TODO
+        // Need to develop obstacle cheking  
+
         //Check if enemy is within angle of turret
         if (currentHorizontalAngle < turretHorizontalAngle && currentVerticalAngle < turretVerticalAngle)
         {
-            //Check if there is no obstacles between enemy and turrete
-            //RaycastHit hit;
-            //if (Physics.Raycast(transform.position, directionToTarget, out hit)
-            //    && hit.transform.tag == enemiesList[0].gameObject.tag)
-            //{
-                return true;
-            //}
+            return true;
         }
 
         return false;
+    }
+
+    private void CalculateTheNearestTarget()
+    {
+        if (targetList.Count == 0) return;
+
+        GameObject nearestTarget = targetList[0];
+        float minDistance = Vector3.Distance(transform.position, targetList[0].transform.position);
+        float currentDistance;
+        int index = 0;
+
+        for (int i = 0; i < targetList.Count - 1; i++)
+        {
+            currentDistance = Vector3.Distance(transform.position, targetList[i].transform.position);
+            if (currentDistance < minDistance)
+            {
+                minDistance = currentDistance;
+                index = i;
+            }
+        }
+
+        if (index != 0)
+        {
+            targetList[0] = targetList[index];
+            targetList[index] = nearestTarget;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (targetTags.Any(tag => other.CompareTag(tag)))
         {
-            enemiesList.Add(other.gameObject);
+            targetList.Add(other.gameObject);
         }
     }
 
@@ -86,7 +113,7 @@ public class TurretScript : MonoBehaviour
     {
         if (targetTags.Any(tag => other.CompareTag(tag)))
         {
-            enemiesList.Remove(other.gameObject);
+            targetList.Remove(other.gameObject);
         }
     }
 }
